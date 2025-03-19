fa26c691b584f6724502452c5ef50a9d51e17047
Tyson404 <25quangt@retsdstudent.ca>
2025-03-19 11:19:21 -0500

    Update TaxCollectorAssignment.java

--- TaxCollectorAssignment.java
+++ TaxCollectorAssignment.java
@@ -14,37 +14,78 @@ public class TaxCollectorAssignment {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int[] array = {1,2,3,4,5};
-        int guess = userChoice(array);
-        System.out.println(guess);
-    }
-    public static int userChoice(int[] ceilingNumber) {
-        int highestNumber = (ceilingNumber.length - 1);
-        System.out.println("guess");
-        while (!scanner.hasNextInt()) {
-            System.out.println("againk");
-            scanner.nextLine();
-        }
-        int choice = scanner.nextInt();
-        while (choice < 1 || choice > ceilingNumber[highestNumber]) {
-            System.out.println("again");
-            scanner.nextLine();
-            choice = scanner.nextInt();
-        }
-        return choice; //placeholder
     }
 
-    public static int[] addToUser(int userScore, int userChoice, int[]numberList) {
-        userScore += userChoice;
+    public static int[] removeChoice(int userChoice, int[]numberList) {
+        //add score in main instead of here
         int[] numberList2;
         numberList2 = new int[numberList.length - 1];
         for (int i = 0, k = 0; i < numberList.length; i++) {
-            if (i == userChoice - 1)
+            if (numberList[i] == userChoice) {
                 continue;
-            
-              numberList2[k++] = numberList[i];
+            }
+            numberList2[k++] = numberList[i];
         }
+        return numberList2;
+    }
 
+    public static int userChoice(int[] numberList) {
+    int choice = 0;
+    int highestNumber = (numberList.length - 1);
+    boolean existsInList = false;
+    System.out.println("guess");
+    try {
+    choice = scanner.nextInt();
+    } catch (Exception m) {}
+    for (int i = 0; i < numberList.length; i ++) {
+        if (choice == numberList[i]) {
+            existsInList = true; 
+        }
+    }
+    while (choice < 1 || choice > numberList[highestNumber] || existsInList == false) {
+        System.out.println("again");
+        System.out.println("choice = " + choice);
+        scanner.nextLine();
+        try {
+        choice = scanner.nextInt();
+        } catch (Exception m) {}
+        for (int i = 0; i < numberList.length; i ++) {
+            if (choice == numberList[i]) {
+                existsInList = true; 
+            }
+        }
+    }
+    scanner.close();
+    return choice;
+    }
+
+    public static int addToCollector(int userChoice, int collectorScore, int[] numberList) { //if noDivisors == true, userChoice will be set to 1 in main
+        for (int i = 0; i < numberList.length; i++) {
+            if (numberList[i] % userChoice == 0) {
+                collectorScore += numberList[i];
+            }
+        }
+        return collectorScore;
+    }
+
+    public static int[] removeDivisors(int userChoice, int[] numberList) {
+        int listSize = 0;
+        int[] numberList2;
+        for (int i = 0; i < numberList.length; i++) {
+            if (numberList[i] % userChoice != 0) {
+                listSize += 1;
+            }
+        }
+        numberList2 = new int[listSize];
+        int k = 0;
+        for (int i = 0; i < numberList.length; i++) {
+            if (numberList[i] % userChoice != 0) {
+                try {
+                numberList2[k] = numberList[i];
+                k += 1;
+                } catch (Exception m) {}
+            }
+        }
         return numberList2;
     }
 }
